368c3826f2a19a5f0ce125b5fb30093bfd9e90b6
Arthur Crocquevieille <arthur.crocquevieille@gmail.com>
2022-04-16 08:45:38 +0200

    Add `ValidateTests`

---
+++ Tests/ValidationTests/ValidateTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import Validation
+
+final class ValidateTests: XCTestCase {
+  func testValidate_Valid() {
+    let predicate1 = Predicate<String> { $0.contains("a") }
+    let predicate2 = Predicate<String> { $0.count == 5 }
+    let validate = Validate {
+      predicate1
+      predicate2
+    }
+    assertValid(validate.validate("abcde")) {
+      XCTAssertEqual($0, "abcde")
+    }
+  }
+  
+  func testValidate_Invalid() {
+    let predicate1 = Predicate<String> { $0.contains("a") }
+    let predicate2 = Predicate<String> { $0.count == 5 }
+    let validate = Validate {
+      predicate1
+      predicate2
+    }
+    assertInvalid(validate.validate("bcd")) {
+      XCTAssertEqual($0.count, 2)
+    }
+  }
+}
